51e8da6c263221765f4d2731a26b0bbc03fa93bc
Andrei1694 <andrei4.stanciu@gmail.com>
2018-10-24 23:52:29 -0700

    Inheritence homework first commit

--- src/Inheritence/Car.java
+++ src/Inheritence/Car.java
@@ -1,4 +1,29 @@
 package Inheritence;
 
 public class Car extends Vehicle {
+    void changingGears(int gear){
+        this.currentGear = gear;
+    }
+
+    void increseSpeed(int newSpeed){
+        switch (currentSpeed){
+            case 30: this.changingGears(2);
+                break;
+            case 50: this.changingGears(3);
+                break;
+            case 70: this.changingGears(4);
+                break;
+            case 100: this.changingGears(5);
+                break;
+        }
+        this.currentSpeed += newSpeed;
+    }
+
+    void decreseSpeed(int newSpeed){
+        this.currentSpeed -= newSpeed;
+    }
+
+    void move(){
+        System.out.println("You're moving with the currentSpeed of "+this.currentSpeed);
+    }
 }
