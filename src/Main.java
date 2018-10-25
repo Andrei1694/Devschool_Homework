0203b3552388ce4ab97b67bf80fcf19c43fa0960
Andrei1694 <andrei4.stanciu@gmail.com>
2018-10-25 01:49:08 -0700

    Started BarkingDog

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import Inheritence.Car;
 public class Main {
 
     public static void main(String[] args) {
-        Car car = new Car();
+//        Car car = new Car();  Inheritence homework
+
     }
 }
